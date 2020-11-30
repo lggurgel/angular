ce46207c3579cc6689c34e06aa9e530f2ead2f08
Luca Gurgel <lucas.gurgel@cocobambu.com>
2020-11-30 17:00:05 -0300

    add course component

--- src/app/course.component.ts
+++ src/app/course.component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses', // <courses>
-    template: '<h2>Courses</h2>'
+    template: `
+        
+        <h2>{{ "Title: " + getTitle() }}</h2 >
+        
+        <ul>
+            <li *ngFor="let course of courses">{{ course }}</li>
+        </ul>
+
+        `
 })
 export class CoursesComponent {
 
+    title = "List of courses";
+    courses = ["course 1", "course 2", "course 3"]
+
+    getTitle() {
+        return this.title;
+    }
 }
