c03fd364708ff572525f40bfec3c6a92681f5e17
Luca Gurgel <lucas.gurgel@cocobambu.com>
2020-12-15 18:00:17 -0300

    add pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './course.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesService } from './course.services';
 import { AuthorsComponent } from './authors/authors.component';
@@ -11,13 +11,13 @@ import { AuthorsComponent } from './authors/authors.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
     CourseComponent,
     AuthorsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CoursesService } from '../course.services';
 
 @Component({
-  selector: 'app-course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.scss']
+    selector: 'courses', // <courses>
+    templateUrl: './course.component.html',
+    styleUrls: ['./course.component.scss']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent {
 
-  constructor() { }
+    course = {
+        title: "The complete Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2020, 11, 11)
+    }
 
-  ngOnInit(): void {
-  }
+    name;
+    title = "List of courses";
+    courses;
 
+    constructor (service: CoursesService){
+        this.courses = service.getCourses()
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    onSave($event) {
+        $event.stopPropagation();
+        console.log("Clicked", $event);
+    }
+
+    onDivClicked($event) {
+        console.log("Div Clicked", $event);
+    }
+
+    onKeyUp(email) {
+        alert("Submitted: " + email);
+    }
+
+    twoWayBiding() {
+        alert("Name: " + this.name)
+    }
 }
